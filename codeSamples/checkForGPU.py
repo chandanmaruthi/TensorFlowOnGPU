74e53a4db435e560285e31f0732cfe66e2cc8776
Chandan Maruthi <chandan.maruthi@gmail.com>
2017-07-22 14:57:05 -0700

    added basic code file checkForGPU

--- codeSamples/checkForGPU.py
+++ codeSamples/checkForGPU.py
@@ -1,3 +1,4 @@
+import tensorflow as tf
 # Creates a graph.
 a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
 b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
