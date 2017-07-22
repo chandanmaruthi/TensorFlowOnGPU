f474fac02d833a95886361c4241a691e2ce7d9cb
Chandan Maruthi <chandan.maruthi@gmail.com>
2017-07-22 15:00:17 -0700

    added basic code file checkForGPU

--- codeSamples/checkForGPU.py
+++ codeSamples/checkForGPU.py
@@ -1,7 +1,8 @@
 import tensorflow as tf
 # Creates a graph.
-a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
-b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
+with tf.device('/cpu:0'):
+    a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
+    b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
 c = tf.matmul(a, b)
 # Creates a session with log_device_placement set to True.
 with tf.Session(config=tf.ConfigProto(log_device_placement=True)) as sess:
